578de6ea6aa5e2c4484480d08cae00aab597f01c
Seokju Hong <iamseokjuhong@gmail.com>
2022-02-17 14:56:52 +0900

    상품 수정

--- jpashop/src/main/java/jpabook/jpashop/service/ItemService.java
+++ jpashop/src/main/java/jpabook/jpashop/service/ItemService.java
@@ -24,7 +24,7 @@ public class ItemService {
         return itemRepository.findAll();
     }
 
-    public Item findIne(Long itemId) {
+    public Item findOne(Long itemId) {
         return itemRepository.findOne(itemId);
     }
 }
