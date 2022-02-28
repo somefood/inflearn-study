bc7fea1a6ac052751f9837cfb55d6360326f2e97
Seokju Hong <iamseokjuhong@gmail.com>
2022-02-28 12:43:28 +0900

    조인

--- jpql/src/main/java/jpql/JpaMain.java
+++ jpql/src/main/java/jpql/JpaMain.java
@@ -16,26 +16,22 @@ public class JpaMain {
 
         try {
 
-            for (int i = 0; i < 100; i++){
-                Member member = new Member();
-                member.setUsername("member" + i);
-                member.setAge(i);
+            Team team = new Team();
+            team.setName("teamA");
 
-                em.persist(member);
-            }
+            Member member = new Member();
+            member.setUsername("member1");
+            member.setAge(10);
+            member.setTeam(team);
+
+            em.persist(member);
 
             em.flush();
             em.clear();
 
-            List<Member> result = em.createQuery("select m from Member m order by m.age desc", Member.class)
-                    .setFirstResult(1)
-                    .setMaxResults(10)
+            String query = "select m from Member m left join m.team t";
+            List<Member> result = em.createQuery(query, Member.class)
                     .getResultList();
-
-            System.out.println("result.size() = " + result.size());
-            for (Member member1 : result) {
-                System.out.println("member1 = " + member1);
-            }
 
             tx.commit();
         } catch (Exception e) {
