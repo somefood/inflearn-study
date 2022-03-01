4f67d8fb554c7b2ead8386b00399eb25e9c9657e
Seokju Hong <iamseokjuhong@gmail.com>
2022-03-01 20:42:23 +0900

    페치 조인 1 - 기본

--- jpql/src/main/java/jpql/JpaMain.java
+++ jpql/src/main/java/jpql/JpaMain.java
@@ -16,34 +16,51 @@ public class JpaMain {
 
         try {
 
-            Team team = new Team();
-            team.setName("teamA");
-            em.persist(team);
+            Team teamA = new Team();
+            teamA.setName("팀A");
+            em.persist(teamA);
+
+            Team teamB = new Team();
+            teamB.setName("팀B");
+            em.persist(teamB);
 
             Member member1 = new Member();
-            member1.setUsername("관리자");
+            member1.setUsername("회원1");
             member1.setAge(10);
-            member1.setTeam(team);
+            member1.setTeam(teamA);
             member1.setMemberType(MemberType.ADMIN);
 
             Member member2 = new Member();
-            member2.setUsername("관리자");
+            member2.setUsername("회원2");
             member2.setAge(10);
-            member2.setTeam(team);
+            member2.setTeam(teamA);
             member2.setMemberType(MemberType.ADMIN);
+
+            Member member3 = new Member();
+            member3.setUsername("회원3");
+            member3.setAge(10);
+            member3.setTeam(teamB);
+            member3.setMemberType(MemberType.ADMIN);
+
+            Member member4 = new Member();
+            member4.setUsername("회원4");
+            member4.setAge(10);
+            member4.setMemberType(MemberType.ADMIN);
 
             em.persist(member1);
             em.persist(member2);
+            em.persist(member3);
+            em.persist(member4);
 
             em.flush();
             em.clear();
 
-            String query = "select m.username from Team t join t.members m";
-            List<String> resultList = em.createQuery(query, String.class)
+            String query = "select distinct t from Team t join fetch t.members where t.name = '팀A'";
+            List<Team> resultList = em.createQuery(query, Team.class)
                     .getResultList();
 
-            for (String s : resultList) {
-                System.out.println("s = " + s);
+            for (Team team : resultList) {
+                System.out.println("team = " + team.getName() + " |members=" + team.getMembers().size());
             }
 
             tx.commit();
