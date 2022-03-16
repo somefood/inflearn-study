5b33332990a8c49fd45a0a35733e407f16fca497
Seokju Hong <iamseokjuhong@gmail.com>
2022-03-16 09:51:09 +0900

    프로젝트 생성

---
+++ tyhmeleaf-basic/src/main/java/hello/tyhmeleaf/TyhmeleafBasicApplication.java
@@ -0,0 +1,13 @@
+package hello.tyhmeleaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TyhmeleafBasicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TyhmeleafBasicApplication.class, args);
+    }
+
+}
---
+++ tyhmeleaf-basic/src/test/java/hello/tyhmeleaf/TyhmeleafBasicApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.tyhmeleaf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TyhmeleafBasicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
