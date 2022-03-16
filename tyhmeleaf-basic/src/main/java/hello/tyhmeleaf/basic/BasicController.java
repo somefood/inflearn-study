1ed22cbfb6756987723ced4f579a1e562c5324d1
Seokju Hong <iamseokjuhong@gmail.com>
2022-03-16 12:03:14 +0900

    URL 링크

--- tyhmeleaf-basic/src/main/java/hello/tyhmeleaf/basic/BasicController.java
+++ tyhmeleaf-basic/src/main/java/hello/tyhmeleaf/basic/BasicController.java
@@ -63,6 +63,13 @@ public class BasicController {
         }
     }
 
+    @GetMapping("link")
+    public String link(Model model) {
+        model.addAttribute("param1", "data1");
+        model.addAttribute("param2", "data2");
+        return "basic/link";
+    }
+
     @GetMapping("/date")
     public String date(Model model) {
         model.addAttribute("localDateTime", LocalDateTime.now());
