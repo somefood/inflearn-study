96e22573e580036a0028dc29702e6b42fb43009e
Seokju Hong <iamseokjuhong@gmail.com>
2022-04-10 19:56:11 +0900

    메소드 이름으로 쿼리 생성

--- data-jpa/src/main/java/study/datajpa/entity/Member.java
+++ data-jpa/src/main/java/study/datajpa/entity/Member.java
@@ -32,6 +32,11 @@ public class Member {
         }
     }
 
+    public Member(String username, int age) {
+        this.username = username;
+        this.age = age;
+    }
+
     public void changeUsername(String username) {
         this.username = username;
     }
