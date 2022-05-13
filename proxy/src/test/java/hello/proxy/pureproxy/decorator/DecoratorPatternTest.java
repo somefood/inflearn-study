fe4822df348700926d2f18838bc74f65f2be52e3
Seokju Hong <iamseokjuhong@gmail.com>
2022-05-13 17:28:41 +0900

    데코레이터 패턴 - 예제 코드3

--- proxy/src/test/java/hello/proxy/pureproxy/decorator/DecoratorPatternTest.java
+++ proxy/src/test/java/hello/proxy/pureproxy/decorator/DecoratorPatternTest.java
@@ -1,8 +1,6 @@
 package hello.proxy.pureproxy.decorator;
 
-import hello.proxy.pureproxy.decorator.code.DecoratorPatternClient;
-import hello.proxy.pureproxy.decorator.code.MessageDecorator;
-import hello.proxy.pureproxy.decorator.code.RealComponent;
+import hello.proxy.pureproxy.decorator.code.*;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 
@@ -21,6 +19,16 @@ public class DecoratorPatternTest {
         RealComponent realComponent = new RealComponent();
         MessageDecorator messageDecorator = new MessageDecorator(realComponent);
         DecoratorPatternClient client = new DecoratorPatternClient(messageDecorator);
+
+        client.execute();
+    }
+
+    @Test
+    void decorator2() {
+        Component realComponent = new RealComponent();
+        Component messageDecorator = new MessageDecorator(realComponent);
+        Component timeDecorator = new TimeDecorator(messageDecorator);
+        DecoratorPatternClient client = new DecoratorPatternClient(timeDecorator);
 
         client.execute();
     }
