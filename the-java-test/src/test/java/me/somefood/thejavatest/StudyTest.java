69c7514b7acc86489b386e5925ba5cf27359ed4c
Seokju Hong <iamseokjuhong@gmail.com>
2022-07-15 13:53:41 +0900

    JUnit 5 테스트 반복하기 1부

--- the-java-test/src/test/java/me/somefood/thejavatest/StudyTest.java
+++ the-java-test/src/test/java/me/somefood/thejavatest/StudyTest.java
@@ -4,6 +4,8 @@ import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.condition.DisabledOnOs;
 import org.junit.jupiter.api.condition.EnabledOnOs;
 import org.junit.jupiter.api.condition.OS;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.time.Duration;
 
@@ -49,6 +51,19 @@ class StudyTest {
         Study study = new Study();
         assertNotNull(study);
         System.out.println("hello1");
+    }
+
+    @DisplayName("스터디 만들기")
+    @RepeatedTest(value = 10, name = "{displayName}, {currentRepetition}/{totalRepetitions}")
+    void repeatTest(RepetitionInfo repetitionInfo) {
+        System.out.println("test" + repetitionInfo.getCurrentRepetition() + "/" + repetitionInfo.getTotalRepetitions());
+    }
+
+    @DisplayName("스터디 만들기")
+    @ParameterizedTest(name = "{index} {displayName} message={0}")
+    @ValueSource(strings = {"날씨가", "많이", "추워지고", "있네요."})
+    void parameterizedTest(String message) {
+        System.out.println("message = " + message);
     }
 
     @BeforeAll
