72d2fb5d5bc192587957f28d2a61f30fa967fd6b
Seokju Hong <iamseokjuhong@gmail.com>
2022-07-15 20:57:12 +0900

    BDD 스타일 Mockito API

--- the-java-test/src/test/java/me/somefood/thejavatest/study/StudyServiceTest.java
+++ the-java-test/src/test/java/me/somefood/thejavatest/study/StudyServiceTest.java
@@ -13,6 +13,8 @@ import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.BDDMockito.then;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
@@ -38,12 +40,17 @@ class StudyServiceTest {
         when(memberService.findById(1L)).thenReturn(Optional.of(member));
         when(studyRepository.save(study)).thenReturn(study);
 
+        // Given
+        given(memberService.findById(1L)).willReturn(Optional.of(member));
+        given(studyRepository.save(study)).willReturn(study);
+
+        // When
         studyService.createNewStudy(1L, study);
 
-        assertEquals(member, study.getOwner());
-
+        // Then
         verify(memberService, times(1)).notify(study);
-        verifyNoMoreInteractions(memberService);
+        then(memberService).should(times(1)).notify(study);
+        then(memberService).shouldHaveNoMoreInteractions();
     }
 
     @Test
