4235248349e00577c7e071978f0ba132211295a9
Seokju Hong <iamseokjuhong@gmail.com>
2022-11-01 18:57:54 +0900

    JdbcTemplate 이름 지정 파라미터 3

--- itemservice-db/src/main/java/hello/itemservice/ItemServiceApplication.java
+++ itemservice-db/src/main/java/hello/itemservice/ItemServiceApplication.java
@@ -10,7 +10,8 @@ import org.springframework.context.annotation.Profile;
 
 
 //@Import(MemoryConfig.class)
-@Import(JdbcTemplateConfigV1.class)
+//@Import(JdbcTemplateConfigV1.class)
+@Import(JdbcTemplateConfigV2.class)
 @SpringBootApplication(scanBasePackages = "hello.itemservice.web")
 public class ItemServiceApplication {
 
