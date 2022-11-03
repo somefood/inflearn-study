78311877ebf768625a1023dba8a2474bd918aa17
Seokju Hong <iamseokjuhong@gmail.com>
2022-11-03 11:41:25 +0900

    JPA 적용1 - 개발

--- itemservice-db/src/main/java/hello/itemservice/ItemServiceApplication.java
+++ itemservice-db/src/main/java/hello/itemservice/ItemServiceApplication.java
@@ -18,7 +18,8 @@ import javax.sql.DataSource;
 //@Import(JdbcTemplateConfigV1.class)
 //@Import(JdbcTemplateConfigV2.class)
 //@Import(JdbcTemplateConfigV3.class)
-@Import(MyBatisConfig.class)
+//@Import(MyBatisConfig.class)
+@Import(JpaConfig.class)
 @SpringBootApplication(scanBasePackages = "hello.itemservice.web")
 public class ItemServiceApplication {
 
