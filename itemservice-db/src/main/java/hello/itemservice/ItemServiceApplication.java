dd629fec19dae483d83e922874d87da4d0e6cd12
Seokju Hong <iamseokjuhong@gmail.com>
2022-11-04 09:52:16 +0900

    Querydsl 적용

--- itemservice-db/src/main/java/hello/itemservice/ItemServiceApplication.java
+++ itemservice-db/src/main/java/hello/itemservice/ItemServiceApplication.java
@@ -20,7 +20,8 @@ import javax.sql.DataSource;
 //@Import(JdbcTemplateConfigV3.class)
 //@Import(MyBatisConfig.class)
 //@Import(JpaConfig.class)
-@Import(SpringDataJpaConfig.class)
+//@Import(SpringDataJpaConfig.class)
+@Import(QuerydslConfig.class)
 @SpringBootApplication(scanBasePackages = "hello.itemservice.web")
 public class ItemServiceApplication {
 
