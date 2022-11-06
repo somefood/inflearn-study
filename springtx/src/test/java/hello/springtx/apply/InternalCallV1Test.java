c8837951bba350b4315459a6e8613b43d9dfbd6e
Seokju Hong <iamseokjuhong@gmail.com>
2022-11-06 20:32:30 +0900

    트랜잭션 AOP 주의 사항 - 프록시 내부 호출2

--- springtx/src/test/java/hello/springtx/apply/InternalCallV1Test.java
+++ springtx/src/test/java/hello/springtx/apply/InternalCallV1Test.java
@@ -1,5 +1,6 @@
 package hello.springtx.apply;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,8 +23,8 @@ public class InternalCallV1Test {
     }
 
     @Test
-    void internalCall() {
-        callService.internal();
+    void externalCallV2() {
+        callService.external();
     }
 
     @Test
@@ -31,28 +32,57 @@ public class InternalCallV1Test {
         callService.external();
     }
 
+    @Test
+    void testCall() {
+        callService.internal();
+    }
+
     @TestConfiguration
     static class InternalCallV1TestConfig {
 
         @Bean
         CallService callService() {
-            return new CallService();
+            return new CallService(internalService());
+        }
+
+        @Bean
+        InternalService internalService() {
+            return new InternalService();
         }
     }
 
     @Slf4j
+    @RequiredArgsConstructor
     static class CallService {
+
+        private final InternalService internalService;
 
         public void external() {
             log.info("call external");
             printTxInfo();
-            internal();
+//            internalService.internal();
         }
 
         @Transactional
         public void internal() {
             log.info("call internal");
+            external();
             printTxInfo();
+        }
+
+        private void printTxInfo() {
+            boolean txActive = TransactionSynchronizationManager.isActualTransactionActive();
+            log.info("tx active={}", txActive);
+            boolean readOnly = TransactionSynchronizationManager.isCurrentTransactionReadOnly();
+            log.info("tx readOnly={}", readOnly);
+        }
+    }
+
+    static class InternalService {
+
+        @Transactional
+        public void internal() {
+            log.info("call internal");
         }
 
         private void printTxInfo() {
