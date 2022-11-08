db96a73c7746b25e445ac2683ac5ef24c20e3832
Seokju Hong <iamseokjuhong@gmail.com>
2022-11-08 17:25:56 +0900

    11) 권한설정과 표현식

--- basicsecurity/src/main/java/io/security/basicsecurity/SecurityConfig.java
+++ basicsecurity/src/main/java/io/security/basicsecurity/SecurityConfig.java
@@ -34,9 +34,21 @@ import java.io.IOException;
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication().withUser("user").password("{noop}1111").roles("USER");
+        auth.inMemoryAuthentication().withUser("sys").password("{noop}1111").roles("SYS", "USER");
+        auth.inMemoryAuthentication().withUser("admin").password("{noop}1111").roles("ADMIN", "SYS", "USER");
+
+    }
+
+    @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
+                .antMatchers("/user").hasRole("USER")
+                .antMatchers("/admin/pay").hasRole("ADMIN")
+                .antMatchers("/admin/**").access("hasRole('ADMIN') or hasRole('SYS')")
                 .anyRequest().authenticated();
+
         http
                 .formLogin();
         http
