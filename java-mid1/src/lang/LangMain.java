40fc6b50b6e1c1cac509a8aebc1aba98193f90a7
somefood <iamseokjuhong@gmail.com>
2024-04-03 12:39:50 +0900

    java.lang 패키지 소개
    lang 패키지는 기본으로 import 해주기에 생략해줘도 됨

---
+++ java-mid1/src/lang/LangMain.java
@@ -0,0 +1,9 @@
+package lang;
+
+//import java.lang.System; // lang 패키지는 기본으로 import 해주기에 생략해줘도 됨
+
+public class LangMain {
+    public static void main(String[] args) {
+        System.out.println("hello java");
+    }
+}
