fadee1c8e9fe11abb942e847cc14a06a0c4733ac
Seokju Hong <iamseokjuhong@gmail.com>
2024-04-12 20:40:45 +0900

    StringBuilder - 가변 String
    StringBuilder는 가변이라 여러 조작을 할 수 있다. 이후 최종적으론 안정적인 String으로 변경해서 사용하자

---
+++ java-mid1/src/lang/string/builder/StringBuilderMain1_1.java
@@ -0,0 +1,27 @@
+package lang.string.builder;
+
+public class StringBuilderMain1_1 {
+
+    public static void main(String[] args) {
+        final StringBuilder sb = new StringBuilder();
+        sb.append("A");
+        sb.append("B");
+        sb.append("C");
+        sb.append("D");
+        System.out.println("sb = " + sb);
+
+        sb.insert(4, "Java");
+        System.out.println("sb = " + sb);
+
+        sb.delete(4, 8);
+        System.out.println("delete = " + sb);
+
+        sb.reverse();
+        System.out.println("reverse = " + sb);
+        
+        // StringBuilder -> String
+        final String string = sb.toString();
+        System.out.println("string = " + string);
+    }
+
+}
