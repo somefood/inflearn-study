50c73e646e9fa32070ec91fbbaf6794e6577c6f0
Seokju Hong <iamseokjuhong@gmail.com>
2024-04-25 20:50:05 +0900

    QueryDSL - 페이징

--- querydsl/src/test/java/me/somefood/querydsl/QUerydslBasicTest.java
+++ querydsl/src/test/java/me/somefood/querydsl/QUerydslBasicTest.java
@@ -146,4 +146,31 @@ public class QUerydslBasicTest {
         assertThat(member6.getUsername()).isEqualTo("member6");
         assertThat(memberNull.getUsername()).isNull();
     }
+
+    @Test
+    void paging1() {
+        final List<Member> result = queryFactory
+            .selectFrom(member)
+            .orderBy(member.username.desc())
+            .offset(1)
+            .limit(2)
+            .fetch();
+
+        assertThat(result.size()).isEqualTo(2);
+    }
+
+    @Test
+    void paging2() {
+        final QueryResults<Member> queryResults = queryFactory
+            .selectFrom(member)
+            .orderBy(member.username.desc())
+            .offset(1)
+            .limit(2)
+            .fetchResults();
+
+        assertThat(queryResults.getTotal()).isEqualTo(4);
+        assertThat(queryResults.getLimit()).isEqualTo(2);
+        assertThat(queryResults.getOffset()).isEqualTo(1);
+        assertThat(queryResults.getResults().size()).isEqualTo(2);
+    }
 }
