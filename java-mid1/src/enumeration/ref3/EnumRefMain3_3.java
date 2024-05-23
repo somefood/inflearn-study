9c23afbe276efa855467278e421996fb0ff07fe0
Seokju Hong <iamseokjuhong@gmail.com>
2024-05-24 00:23:57 +0900

    열거형 - 리팩토링3

---
+++ java-mid1/src/enumeration/ref3/EnumRefMain3_3.java
@@ -0,0 +1,21 @@
+package enumeration.ref3;
+
+import static enumeration.ref3.Grade.BASIC;
+import static enumeration.ref3.Grade.DIAMOND;
+import static enumeration.ref3.Grade.GOLD;
+
+public class EnumRefMain3_3 {
+    
+    public static void main(String[] args) {
+        int price = 10000;
+
+        printDiscount(BASIC, price);
+        printDiscount(GOLD, price);
+        printDiscount(DIAMOND, price);
+    }
+
+    private static void printDiscount(Grade grade, int price) {
+        System.out.println(grade.name() + " 등급의 할인 금액: " + grade.discount(price));
+
+    }
+}
