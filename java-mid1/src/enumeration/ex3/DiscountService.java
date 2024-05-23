6685fd279120f88c832ece13feea6d4d3f4ee0ec
Seokju Hong <iamseokjuhong@gmail.com>
2024-05-23 23:53:38 +0900

    열거형 - Enum Type

---
+++ java-mid1/src/enumeration/ex3/DiscountService.java
@@ -0,0 +1,26 @@
+
+package enumeration.ex3;
+
+import static enumeration.ex3.Grade.BASIC;
+import static enumeration.ex3.Grade.DIAMOND;
+import static enumeration.ex3.Grade.GOLD;
+
+public class DiscountService {
+    
+    public int discount(Grade grade, int price) {
+        int discountPercent = 0;
+
+        if (grade == BASIC) {
+            discountPercent = 10;
+        } else if (grade == GOLD) {
+            discountPercent = 20;
+        } else if (grade == DIAMOND) {
+            discountPercent = 30;
+        } else {
+            System.out.println("할인X");
+        }
+        
+        // 10000 * (20 / 100) -> 2000
+        return price * discountPercent / 100;
+    }
+}
---
+++ java-mid1/src/enumeration/ex3/StringGradeEx3_2.java
@@ -0,0 +1,13 @@
+package enumeration.ex3;
+
+public class StringGradeEx3_2 {
+
+    public static void main(String[] args) {
+        int price = 10000;
+
+        DiscountService discountService = new DiscountService();
+
+//        final Grade grade = new Grade(); // enum 생성 불가
+//        discountService
+    }
+}
