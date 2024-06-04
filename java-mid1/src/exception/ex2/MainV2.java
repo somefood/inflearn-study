d69dab4bb0d600e01323797713497454893927ec
somefood <iamseokjuhong@gmail.com>
2024-06-04 12:29:50 +0900

    예외 처리 도입3 - 정상, 예외 흐름 분리

--- java-mid1/src/exception/ex2/MainV2.java
+++ java-mid1/src/exception/ex2/MainV2.java
@@ -7,7 +7,8 @@ public class MainV2 {
     public static void main(String[] args) throws NetworkClientExceptionV2 {
 //        final NetworkServiceV1_1 networkService = new NetworkServiceV1_1();
 //        final NetworkServiceV2_1 networkService = new NetworkServiceV2_1();
-        final NetworkServiceV2_2 networkService = new NetworkServiceV2_2();
+//        final NetworkServiceV2_2 networkService = new NetworkServiceV2_2();
+        final NetworkServiceV2_3 networkService = new NetworkServiceV2_3();
 
         final Scanner scanner = new Scanner(System.in);
         while (true) {
