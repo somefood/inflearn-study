43d560ea7e4d37f00ae27fced8616ee4bc92d24c
somefood <iamseokjuhong@gmail.com>
2024-06-04 11:46:33 +0900

    예외 처리 도입1 - 시작

---
+++ java-mid1/src/exception/ex2/MainV2.java
@@ -0,0 +1,24 @@
+package exception.ex2;
+
+import java.util.Scanner;
+
+public class MainV2 {
+
+    public static void main(String[] args) throws NetworkClientExceptionV2 {
+//        final NetworkServiceV1_1 networkService = new NetworkServiceV1_1();
+//        final NetworkServiceV1_2 networkService = new NetworkServiceV1_2();
+        final NetworkServiceV2_1 networkService = new NetworkServiceV2_1();
+
+        final Scanner scanner = new Scanner(System.in);
+        while (true) {
+            System.out.print("전송할 문자: ");
+            final String input = scanner.nextLine();
+            if (input.equals("exit")) {
+                break;
+            }
+            networkService.sendMessage(input);
+            System.out.println();
+        }
+        System.out.println("프로그램을 정상 종료합니다.");
+    }
+}
