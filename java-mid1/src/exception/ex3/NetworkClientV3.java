86912c408904102eaeba62b6b259c3ea1eb6557f
somefood <iamseokjuhong@gmail.com>
2024-06-05 12:13:43 +0900

    예외 계층1 - 시작

---
+++ java-mid1/src/exception/ex3/NetworkClientV3.java
@@ -0,0 +1,47 @@
+package exception.ex3;
+
+public class NetworkClientV3 {
+
+    private final String address;
+    public boolean connectError; // default false
+    public boolean sendError;
+
+    public NetworkClientV3(String address) {
+        this.address = address;
+    }
+
+    public void connect() throws ConnectExceptionV3 {
+        if (connectError) {
+            throw new ConnectExceptionV3(address, address + " 서버 연결 실패");
+        }
+
+        // 연결 성공
+        System.out.println(address + " 서버 연결 성공");
+    }
+
+    public void send(String data) throws SendExceptionV3 {
+        if (sendError) {
+            throw new SendExceptionV3(data, address + " 서버에 데이터 전송 실패: " + data);
+
+            // 생각치 못한 예외 발생
+            // throw new RuntimeException();
+        }
+
+        // 전송 성공
+        System.out.println(address + " 서버에 데이터 전송: " + data);
+    }
+
+    public void disconnect() {
+        System.out.println(address + " 서버 연결 해제");
+    }
+
+    public void initError(String data) {
+        if (data.contains("error1")) {
+            connectError = true;
+        }
+
+        if (data.contains("error2")) {
+            sendError = true;
+        }
+    }
+}
