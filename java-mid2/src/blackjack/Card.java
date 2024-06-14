221e748fe88c990204a9f76e976276a87c9f7863
somefood <iamseokjuhong@gmail.com>
2024-06-14 17:31:47 +0900

    외전 - 블랙잭 게임 구현(3)

--- java-mid2/src/blackjack/Card.java
+++ java-mid2/src/blackjack/Card.java
@@ -3,10 +3,33 @@ package blackjack;
 public class Card {
     private String pattern;
     private String denomination;
+    private int point;
 
-    public Card(String pattern, String denomination) {
+    public Card(String pattern, int index) {
         this.pattern = pattern;
-        this.denomination = denomination;
+        this.denomination = this.numberToDenomination(index);
+        this.point = this.numberToPoint(index);
+    }
+
+    private int numberToPoint(int number) {
+        if (number >= 11) {
+            return 10;
+        }
+        return number;
+    }
+
+    private String numberToDenomination(int number) {
+        if (number == 1) {
+            return "A";
+        } else if(number == 11) {
+            return "J";
+        } else if(number == 12) {
+            return "Q";
+        } else if(number == 13) {
+            return "K";
+        } else {
+            return String.valueOf(number);
+        }
     }
 
     public String getPattern() {
@@ -23,6 +46,10 @@ public class Card {
     
     public void setDenomination(String denomination) {
         this.denomination = denomination;
+    }
+
+    public int getPoint() {
+        return point;
     }
 
     @Override
--- java-mid2/src/blackjack/Game.java
+++ java-mid2/src/blackjack/Game.java
@@ -6,11 +6,14 @@ public class Game {
 
     private static final int INIT_RECEIVE_CARD_COUNT = 2;
 
-    private void initPhase(CardDeck cardDeck, Gamer gamer) {
+    private void initPhase(CardDeck cardDeck, Gamer gamer, Dealer dealer) {
         System.out.println("처음 2장의 카드를 각자 뽑겠습니다.");
         for (int i = 0; i < INIT_RECEIVE_CARD_COUNT; i++) {
             Card card = cardDeck.draw();
             gamer.receiveCard(card);
+
+            Card card2 = cardDeck.draw();
+            dealer.receiveCard(card2);
         }
     }
 
@@ -18,28 +21,45 @@ public class Game {
     public void play() {
         System.out.println("======== Blackjack Game =======");
         Scanner sc = new Scanner(System.in);
-        
+
         Dealer dealer = new Dealer();
         Gamer gamer = new Gamer();
         Rule rule = new Rule();
         CardDeck cardDeck = new CardDeck();
 
-        initPhase(cardDeck, gamer);
-        playingPhase(sc, cardDeck, gamer);
+        initPhase(cardDeck, gamer, dealer);
+        playingPhase(sc, cardDeck, gamer, dealer);
     }
 
-    private void playingPhase(Scanner sc, CardDeck cardDeck, Gamer gamer) {
-        String gamerInput;
+    private void playingPhase(Scanner sc, CardDeck cardDeck, Gamer gamer, Dealer dealer) {
+        String gamerInput, dealerInput;
+        boolean isGamerTurn = false,
+                isDealerTurn = false;
+
         while (true) {
             System.out.println("카드를 뽑겠습니까? 종료를 원하시면 0을 입력하세요.");
             gamerInput = sc.nextLine();
-            
+
             if ("0".equals(gamerInput)) {
-                break;
+                isGamerTurn = true;
+            } else {
+                Card card = cardDeck.draw();
+                gamer.receiveCard(card);
             }
 
-            Card card = cardDeck.draw();
-            gamer.receiveCard(card);
+            System.out.println("카드를 뽑겠습니까? 종료를 원하시면 0을 입력하세요.");
+            dealerInput = sc.nextLine();
+
+            if ("0".equals(dealerInput)) {
+                isDealerTurn = true;
+            } else {
+                Card card = cardDeck.draw();
+                dealer.receiveCard(card);
+            }
+
+            if (isGamerTurn && isDealerTurn) {
+                break;
+            }
         }
     }
 }
