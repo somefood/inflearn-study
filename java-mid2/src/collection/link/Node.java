25003a528ea4e4f8f78e617c8dd6aecfafc616e4
somefood <iamseokjuhong@gmail.com>
2024-06-14 12:25:46 +0900

    노드와 연결2

--- java-mid2/src/collection/link/Node.java
+++ java-mid2/src/collection/link/Node.java
@@ -8,4 +8,29 @@ public class Node {
     public Node(Object item) {
         this.item = item;
     }
+
+    // IDE 생성
+//    @Override
+//    public String toString() {
+//        return "Node{" +
+//                "item=" + item +
+//                ", next=" + next +
+//                '}';
+//    }
+    
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder(); // loop인 경우 String Builder 쓰자~!
+        Node x = this;
+        sb.append("[");
+        while (x != null) {
+            sb.append(x.item);
+            if (x.next != null) {
+                sb.append("->");
+            }
+            x = x.next;
+        }
+        sb.append("]");
+        return sb.toString();
+    }
 }
