6d77facca15c81788f6dea8a1c758cc1a216bf27
somefood <iamseokjuhong@gmail.com>
2024-09-25 12:32:23 +0900

    자바 고급1편 - 생산자 소비자 대기 공간 분리

--- java-adv1/src/thread/bounded/BoundedQueueV5.java
+++ java-adv1/src/thread/bounded/BoundedQueueV5.java
@@ -8,15 +8,16 @@ import java.util.concurrent.locks.ReentrantLock;
 
 import static util.MyLogger.log;
 
-public class BoundedQueueV4 implements BoundedQueue {
+public class BoundedQueueV5 implements BoundedQueue {
 
     private final Lock lock = new ReentrantLock();
-    private final Condition condition = lock.newCondition(); // 대기 집합소
+    private final Condition producerCond = lock.newCondition();
+    private final Condition consumerCond = lock.newCondition();
     
     private final Queue<String> queue = new ArrayDeque<>();
     private final int max;
 
-    public BoundedQueueV4(int max) {
+    public BoundedQueueV5(int max) {
         this.max = max;
     }
 
@@ -27,15 +28,15 @@ public class BoundedQueueV4 implements BoundedQueue {
             while (queue.size() == max) {
                 log("[put] 큐가 가득 참, 생산자 대기");
                 try {
-                    condition.await();
+                    producerCond.await();
                     log("[put] 생산자 깨어남");
                 } catch (InterruptedException e) {
                     throw new RuntimeException(e);
                 }
             }
             queue.offer(data);
-            log("[put] 생산자 데이터 저장, signal() 호출");
-            condition.signal();
+            log("[put] 생산자 데이터 저장, consumerCond.signal() 호출");
+            consumerCond.signal();
         } finally {
             lock.unlock();
         }
@@ -48,7 +49,7 @@ public class BoundedQueueV4 implements BoundedQueue {
             while (queue.isEmpty()) {
                 log("[take] 큐에 데이터가 없음, 소비자 대기");
                 try {
-                    condition.await();
+                    consumerCond.await();
                     log("[take] 소비자 깨어남");
                 } catch (InterruptedException e) {
                     throw new RuntimeException(e);
@@ -56,8 +57,8 @@ public class BoundedQueueV4 implements BoundedQueue {
             }
 
             final String data = queue.poll();
-            log("[take] 소비자 데이터 획득, signal() 호출");
-            condition.signal();
+            log("[take] 소비자 데이터 획득, producerCond.signal() 호출");
+            producerCond.signal();
             return data;
         } finally {
             lock.unlock();
