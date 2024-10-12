ff4082ed25f78b180e9d42e145befbf96ace7aac
Seokju Hong <iamseokjuhong@gmail.com>
2024-10-12 13:24:33 +0900

    Practical Testing: 실용적인 테스트 가이드 - TDD

--- cafekiosk/src/test/java/sample/cafekiosk/unit/CafeKioskTest.java
+++ cafekiosk/src/test/java/sample/cafekiosk/unit/CafeKioskTest.java
@@ -79,6 +79,20 @@ class CafeKioskTest {
     }
 
     @Test
+    void calculateTotalPrice() {
+        CafeKiosk cafeKiosk = new CafeKiosk();
+        final Americano americano = new Americano();
+        final Latte latte = new Latte();
+
+        cafeKiosk.add(americano);
+        cafeKiosk.add(latte);
+
+        int totalPrice = cafeKiosk.calculateTotalPrice();
+
+        assertThat(totalPrice).isEqualTo(8500);
+    }
+
+    @Test
     void createOrder() {
         CafeKiosk cafeKiosk = new CafeKiosk();
         final Americano americano = new Americano();
