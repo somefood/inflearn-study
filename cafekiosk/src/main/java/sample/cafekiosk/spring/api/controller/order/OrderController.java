6368277045e9e49c77849a786ea1a1325050da65
Seokju Hong <iamseokjuhong@gmail.com>
2024-10-16 01:06:36 +0900

    Practical Testing: 실용적인 테스트 가이드 - Business Layer테스트(1)

---
+++ cafekiosk/src/main/java/sample/cafekiosk/spring/api/controller/order/OrderController.java
@@ -0,0 +1,22 @@
+package sample.cafekiosk.spring.api.controller.order;
+
+import java.time.LocalDateTime;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import sample.cafekiosk.spring.api.controller.order.request.OrderCreateRequest;
+import sample.cafekiosk.spring.api.service.order.OrderService;
+
+@RequiredArgsConstructor
+@RestController
+public class OrderController {
+
+    private final OrderService orderService;
+
+    @PostMapping("/api/v1/orders/new")
+    public void createOrder(@RequestBody OrderCreateRequest request) {
+        final LocalDateTime registeredDateTime = LocalDateTime.now();
+        orderService.createOrder(request, registeredDateTime);
+    }
+}
