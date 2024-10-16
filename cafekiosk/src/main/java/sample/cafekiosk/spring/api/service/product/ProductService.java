3f20c7b0b8e84f89ed581d330facbaa84a9de188
Seokju Hong <iamseokjuhong@gmail.com>
2024-10-17 00:09:38 +0900

    Practical Testing: 실용적인 테스트 가이드 - Presentation Layer 테스트(1)

--- cafekiosk/src/main/java/sample/cafekiosk/spring/api/service/product/ProductService.java
+++ cafekiosk/src/main/java/sample/cafekiosk/spring/api/service/product/ProductService.java
@@ -1,27 +1,60 @@
 package sample.cafekiosk.spring.api.service.product;
 
+import java.util.List;
+import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import sample.cafekiosk.spring.api.controller.product.dto.request.ProductCreateRequest;
 import sample.cafekiosk.spring.api.service.product.response.ProductResponse;
 import sample.cafekiosk.spring.domain.product.Product;
 import sample.cafekiosk.spring.domain.product.ProductRepository;
 import sample.cafekiosk.spring.domain.product.ProductSellingStatus;
 
-import java.util.List;
-import java.util.stream.Collectors;
-
+/**
+ * readOnly = true: 읽기전용 트랜잭션 동작
+ * CRUD 에서 CUD 동작 X / ONLY READ
+ * JPA : CUD 스냅샷 저장, 변경감지 X (성능 향상)
+ *
+ * CQRS - Command / Read
+ */
+@Transactional(readOnly = true)
 @RequiredArgsConstructor
 @Service
 public class ProductService {
     
     private final ProductRepository productRepository;
-    
+
+    // 동시성 이슈 발생할 여지가 다분
+    // UUID 활용
+    @Transactional
+    public ProductResponse createProduct(ProductCreateRequest request) {
+        final String nextProductNumber = createNextProductNumber();
+
+        Product product = request.toEntity(nextProductNumber);
+        final Product savedProduct = productRepository.save(product);
+
+        return ProductResponse.of(savedProduct);
+    }
+
     public List<ProductResponse> getSellingProducts() {
         List<Product> products = productRepository.findAllBySellingStatusIn(ProductSellingStatus.forDisplay());
-        
+
         return products.stream()
                 .map(ProductResponse::of)
                 .collect(Collectors.toList());
 
+    }
+
+    private String createNextProductNumber() {
+        String latestProductNumber = productRepository.findLatestProductNumber();
+        if (latestProductNumber == null) {
+            return "001";
+        }
+
+        int latestProductNumberInt = Integer.parseInt(latestProductNumber);
+        int nextProductNumberInt = latestProductNumberInt + 1;
+
+        return String.format("%03d", nextProductNumberInt);
     }
 }
