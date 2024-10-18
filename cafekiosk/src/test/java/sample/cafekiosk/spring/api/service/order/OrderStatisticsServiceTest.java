0055a4d90a78352523f10c68c2497d2b618f2e36
somefood <iamseokjuhong@gmail.com>
2024-10-18 12:29:19 +0900

    Practical Testing: 실용적인 테스트 가이드 - @Mock, @Spy, @InjectMocks

--- cafekiosk/src/test/java/sample/cafekiosk/spring/api/service/order/OrderStatisticsServiceTest.java
+++ cafekiosk/src/test/java/sample/cafekiosk/spring/api/service/order/OrderStatisticsServiceTest.java
@@ -12,7 +12,6 @@ import java.util.List;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
